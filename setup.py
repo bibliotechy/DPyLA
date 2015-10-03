2819ba75cf6811eb54503012669b4b58dec7044a
Chad Nelson <chadbnelson@gmail.com>
2015-10-03 14:23:52 -0400

    Use new github tarbal url

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     author = 'Chad Nelson',
     author_email = 'chadbnelson@gmail.com',
     url = 'https://github.com/bibliotechy/DPyLA',   # use the URL to the github repo
-    download_url = 'https://github.com/bibliotechy/DPyLA/tarball/0.2', # I'll explain this in a second
+    download_url = 'https://github.com/bibliotechy/DPyLA/archive/0.2.tar.gz',
     keywords = ['libraries', 'DPLA', 'museums'], # arbitrary keywords
     classifiers = [],
     install_requires = [ 'requests' ]
