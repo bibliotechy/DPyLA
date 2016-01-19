3d164f3929e7501b70f0d631e9941a4ec5693eb5
Chad Nelson <chad.nelson@temple.edu>
2016-01-19 15:58:46 -0500

    Bump to version 0.3

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup
 setup(
     name = 'dpla',
     packages = ['dpla'], # this must be the same as the name above
-    version = '0.2',
+    version = '0.3',
     description = 'A python client for the DPLA API',
     author = 'Chad Nelson',
     author_email = 'chadbnelson@gmail.com',
     url = 'https://github.com/bibliotechy/DPyLA',   # use the URL to the github repo
-    download_url = 'https://github.com/bibliotechy/DPyLA/archive/0.2.tar.gz',
+    download_url = 'https://github.com/bibliotechy/DPyLA/archive/0.3.tar.gz',
     keywords = ['libraries', 'DPLA', 'museums'], # arbitrary keywords
     test_suite='tests',
     classifiers = [],
