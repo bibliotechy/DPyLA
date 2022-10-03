2936c0167bc119f1088aab25ca50a89bed47f935
Chad Nelson <chad@archive.org>
2022-10-03 10:25:21 -0400

    Add pyproject.toml for releases

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from setuptools import setup
 setup(
     name="dpla",
     packages=["dpla"],  # this must be the same as the name above
-    version="0.4",
+    version="0.5",
     description="A python client for the DPLA API",
     author="Chad Nelson",
     author_email="chadbnelson@gmail.com",
     url="https://github.com/bibliotechy/DPyLA",  # use the URL to the github repo
-    download_url="https://github.com/bibliotechy/DPyLA/archive/0.4.tar.gz",
+    download_url="https://github.com/bibliotechy/DPyLA/releases/latest",
     keywords=["libraries", "DPLA", "museums"],  # arbitrary keywords
-    test_suite="tests",
+    test_suite="tests.py",
     classifiers=[],
     install_requires=["requests"],
 )
